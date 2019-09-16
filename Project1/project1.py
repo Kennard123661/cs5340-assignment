5d443b21fcc9624e51e61620ba4f4d3e1243531f
Kennard Ng <kennard_ng@outlook.com>
2019-09-16 15:39:24 +0800

    conver to per image computation

--- Project1/project1.py
+++ Project1/project1.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 import sys
-import math
+import copy
 import numpy as np
 
 
@@ -16,88 +16,96 @@ image_filepaths = [os.path.join(base_dir, image_filename) for image_filename in 
 camera_parameters_filepath = os.path.join(base_dir, 'cameraParameters.mat')
 
 result_dir = os.path.join(base_dir, 'results')
-preprocess_dir = os.path.join(result_dir, 'gray_scale')
+grayscale_dir = os.path.join(result_dir, 'grayscale')
+pixel_dir = os.path.join(result_dir, 'pixel')
+
+VANISHING_POINT_DIRECTIONS = [[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0]]
+EDGE_MODELS_PRIOR = [0.02, 0.02, 0.02, 0.04, 0.09]
 
 
-def read_images(filepaths):
-    return [cv2.imread(filepath) for filepath in filepaths]
+def read_image(filename):
+    filepath = os.path.join(base_dir, filename)
+    return cv2.imread(filepath)
 
 
-def get_grayscale_images(images):
+def get_grayscale_image(image):
     """ Converts the image to grayscale and downsamples the image by resizing by a factor of 0.2 """
-    grayscale_images = list()
-    for image in images:
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        grayscale_images.append(gray_image)
-    return grayscale_images
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return grayscale_image
 
 
 def get_em_pixel_idxs(grad_mags, grad_directions):
-    downsampled_idxs = list()
-    for i, grad_mag in enumerate(grad_mags):
-        grad_direction = grad_directions[i]
-        _, downsampled_idx = helper_functions.down_sample(Gmag_=grad_mag, Gdir_=grad_direction)
-        downsampled_idxs.append(downsampled_idx)
+    _, downsampled_idxs = helper_functions.down_sample(Gmag_=grad_mags, Gdir_=grad_directions)
     downsampled_idxs = np.array(downsampled_idxs)
     return downsampled_idxs
 
 
-def save_image(image, filepath):
-    save_dir = os.path.dirname(filepath)
+def save_image(image, save_dir, save_filename):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
+    filepath = os.path.join(save_dir, save_filename)
     cv2.imwrite(filepath, image)
 
 
-def save_images(images, save_filenames, save_dir):
-    assert len(images) == len(save_filenames)
-    for i, image in enumerate(images):
-        save_filepath = os.path.join(save_dir, save_filenames[i])
-        save_image(image, save_filepath)
+def get_image_gradients(image):
+    grad_x = np.array(cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5))
+    grad_y = np.array(cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5))
+
+    # grad directions represent as (cos\theta, sin\theta)
+    grad_mag = np.sqrt(np.square(np.array(grad_x)) + np.square(np.array(grad_y)))
+    grad_direction = np.concatenate([np.expand_dims(grad_x, axis=-1), np.expand_dims(grad_y, axis=-1)], axis=-1)
+    return np.array(grad_mag), np.array(grad_direction)
 
 
-def get_grad_direction(x_grads, y_grads):
-    directions = np.zeros_like(x_grads)
-    assert x_grads.shape == y_grads.shape
-    r, c = x_grads.shape
-    for i in range(r):
-        for j in range(c):
-            x_grad = x_grads[i, j]
-            y_grad = y_grads[i, j]
-            if x_grad == 0:  # implies that the direction is along the y-axis
-                directions[i, j] = math.pi / 2
-            elif y_grad == 0:  # implies that it is along the x-axis
-                directions[i, j] = 0
-            else:  # normal scenario
-                directions[i, j] = math.atan(y_grad / x_grad)
-    return directions
+def get_initial_rot_estimate(camera_intrinsics, pixel_grad_mags, pixel_grad_directions):
+    """ Reimplementation of https://pdfs.semanticscholar.org/3f12/20be9e783caa716482863af4a671197c6aeb.pdf """
+    a, g = 0, 0
+    for b in range(-45, 46, 4):
+        rot_matrix = helper_functions.angle2matrix(a, b, g)
+        vanishing_points = np.matmul(camera_intrinsics, np.matmul(rot_matrix, VANISHING_POINT_DIRECTIONS))
+        print(vanishing_points)
+        break
+    return 1
 
 
-def get_gradients(images):
-    grad_mags, grad_directions = list(), list()
-    for image in images:
-        grad_x = np.array(cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5))
-        grad_y = np.array(cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5))
-        grad_mag = np.sqrt(np.square(np.array(grad_x)) + np.square(np.array(grad_y)))
-        grad_direction = get_grad_direction(grad_x, grad_y)
-        grad_mags.append(grad_mag)
-        grad_directions.append(grad_direction)
-    return np.array(grad_mags), np.array(grad_directions)
+def annotate_pixel_locations(image, pixel_locations, save_filename, region_size=1, color=(0, 0, 255)):
+    image_to_annotate = copy.deepcopy(image)
+    locations_to_annotate = copy.deepcopy(pixel_locations) * 5 + 4
+    image_width, image_height, _ = image.shape
+    for u, v in locations_to_annotate:
+        for i in range(u-region_size, min(u+region_size+1, image_width-1)):
+            for j in range(v-region_size, min(v+region_size+1, image_height-1)):
+                image_to_annotate[i, j] = color
+    save_image(image_to_annotate, save_dir=pixel_dir, save_filename=save_filename)
 
 
-def main():
-    images = read_images(image_filepaths)
-    camera_params = helper_functions.cam_intrinsics(camera_parameters_filepath)
+def process_image(image_filename):
+    image = read_image(image_filename)
+    camera_intrinsics = helper_functions.cam_intrinsics(camera_parameters_filepath)
 
     # Step 1: preprocess.
-    grayscale_images = get_grayscale_images(images)
-    save_images(grayscale_images, image_filenames, save_dir=preprocess_dir)
-    grad_mags, grad_directions = get_gradients(grayscale_images)
+    grayscale_image = get_grayscale_image(image)
+    save_image(grayscale_image, save_dir=grayscale_dir, save_filename=image_filename)
+    grad_mags, grad_directions = get_image_gradients(grayscale_image)
+    pixel_idxs = get_em_pixel_idxs(grad_mags=grad_mags, grad_directions=grad_directions)
+    annotate_pixel_locations(image, pixel_idxs, image_filename)
 
-    em_pixel_idxs = get_em_pixel_idxs(grad_mags=grad_mags, grad_directions=grad_directions)
+    pixel_grad_mags, pixel_grad_directions = get_pixel_gradients(grad_mags, grad_directions, pixel_idxs)
+    print(pixel_grad_mags.shape)
+    print(pixel_grad_directions.shape)
+    rot_estimate = get_initial_rot_estimate(camera_intrinsics, pixel_grad_mags, pixel_grad_directions)
 
-    print(camera_params)
+
+def get_pixel_gradients(grad_mags, grad_directions, pixel_idxs):
+    downsampled_grad_directions = grad_directions[4::5]
+    downsampled_grad_mags = grad_mags[4::5]
+    pixel_grad_mags, pixel_grad_directions = list(), list()
+    for i, j in pixel_idxs:
+        pixel_grad_mags.append(downsampled_grad_mags[i, j])
+        pixel_grad_directions.append(downsampled_grad_directions[i, j])
+    return np.array(pixel_grad_mags), np.array(pixel_grad_directions)
 
 
 if __name__ == '__main__':
-    main()
+    for image_filename in image_filenames:
+        process_image(image_filename)
